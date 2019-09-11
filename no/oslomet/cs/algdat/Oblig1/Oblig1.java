7a1ed0d0f453777b4539f0f5d997dbf6a768e1a5
Asso Rostampoor <asso.rostampoor@gmail.com>
2019-09-11 12:20:58 +0200

    tester

--- no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -25,7 +25,7 @@ public class Oblig1 {
     public static int antallUlikeSortert(int[] a) {
         throw new NotImplementedException();
     }
-    // Asso tar denne
+    // Asso branch
 
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
