5b1893cecb369b2b39029a9a3ba18eb630fc189e
LindusheK <s333751@oslomet.no>
2019-09-16 00:28:58 +0200

    Oppgave 7b) ferdig, mangler NotImplementedException

--- no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -50,7 +50,9 @@ public class Oblig1 {
     ///// Oppgave 7 //////////////////////////////////////
     /// 7a)
     public static String flett(String s, String t) {
-        //throw new NotImplementedException();
+        if (s == null || t == null) {
+            throw new NotImplementedException();
+        }
 
         //her konvereter vi strings til array
         //lengden på arrayet vil være like langt som lengden på stringen
@@ -76,8 +78,8 @@ public class Oblig1 {
         // 3) arrayet det skal kopieres til
         // 4) start posisjon i arrayet kopiert til
         // 5) total antall komponenter som skal kopieres
-        System.arraycopy(a1, b, flettetStrings, 2*b, a1.length -1);
-        System.arraycopy(a2, b, flettetStrings, 2*b, a2.length -1);
+        System.arraycopy(a1, b, flettetStrings, 2 * b, a1.length - 1);
+        System.arraycopy(a2, b, flettetStrings, 2 * b, a2.length - 1);
 
         //Gjør om char arrayet flettetStrings til en string. ValueOf() metode som lar oss konvertere
         //til de fleste typer av string.
@@ -85,9 +87,47 @@ public class Oblig1 {
     }
 
     /// 7b)
-    public static String flett(String... s) {
-        throw new NotImplementedException();
+    public static String flett(String[] s) {
+        //throw new NotImplementedException();
+
+        //hvis lengden til s er 0 (ingen verdier) vil den retunerer tom string.
+        if (s.length == 0) {
+            return "";
+        }
+
+        //int a er første streng i tabellen s og retunerer lengden
+        int a = s[0].length();
+
+        //for løkke gjennom arrayet
+        for (int i = 1; i < s.length; i++) {
+            //lengde er i posisjon i arrayet s og returnert olengde
+            int lengde = s[i].length();
+
+            //hvis lengde er større enn a, som vi først fant,
+            //så skal a oppdateres
+            if (lengde > a) {
+                a = lengde;
+            }
+        }
+
+        //lager ny stringbuilder, har startkapasitet på 16 tegn.
+        StringBuilder Stringbuilder = new StringBuilder();
+
+        //for løkke som går gjennom lengden
+        for (int i = 0; i < a; i++) {
+            //for alle string verdder i arrayet
+            for (String t : s) {
+                //hvis i er mindre enn veridene
+                if (i < t.length()) {
+                    //legger til strenger av et argument til sekvensen.
+                    //charAt() = skriver ut alle tegnenen i en streng, viser tegn ved hver iterasjon av løkken
+                    Stringbuilder.append(t.charAt(i));
+                }
+            }
+        }
+        return Stringbuilder.toString();
     }
+
 
     ///// Oppgave 8 //////////////////////////////////////
     public static int[] indekssortering(int[] a) {
